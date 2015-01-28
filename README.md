7a645771cba86eee366e2e118270ed45c8ce2a45
Alex Gusev <flancer64@gmail.com>
2015-01-28 13:23:53 +0200

    SUPP-434 - Nmmlm_Log refactoring to use with Magento Composer

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# mage_ext_log4php
-Magento extension: Log4php wrapper to use Log4php with Magento
+# Log4php wrapper to use Log4php with Magento
+
+## Usage
+
+### Create adapter in your own module
+
+This adapter uses Nmmlm_Log wrapper or Magento default logs:
+
+    <?php
+    class Nmmlm_Core_Logger
+    {
+        /** @var bool 'true' - Log4php logging framework is used. */
+        private static $_isLog4phpUsed = null;
+        /** @var Nmmlm_Log_Logger */
+        private $_loggerLog4php;
+        /** @var string name for the current logger */
+        private $_name;
+    
+        function __construct($name)
+        {
+            try {
+                /** load PHP class if not loaded yet */
+                new Nmmlm_Log_Logger('just probe');
+            } catch (Exception $e) {
+            }
+            self::$_isLog4phpUsed = class_exists('Nmmlm_Log_Logger', false);
+            if (self::$_isLog4phpUsed) {
+                $this->_loggerLog4php = Nmmlm_Log_Logger::getLogger($name);
+            } else {
+                $this->_name = is_object($name) ? get_class($name) : (string)$name;
+            }
+        }
+    
+        /**
+         * Override getter to use '$log = Nmmlm_Log_Logger::getLogger($this)' form in Mage classes.
+         * @static
+         *
+         * @param string $name
+         *
+         * @return Nmmlm_Core_Logger
+         */
+        public static function getLogger($name)
+        {
+            $class = __CLASS__;
+            return new $class($name);
+        }
+    
+        public function debug($message, $throwable = null)
+        {
+            $this->doLog($message, $throwable, 'debug', Zend_Log::INFO);
+        }
+    
+        public function error($message, $throwable = null)
+        {
+            $this->doLog($message, $throwable, 'error', Zend_Log::ERR);
+        }
+    
+        public function fatal($message, $throwable = null)
+        {
+            $this->doLog($message, $throwable, 'fatal', Zend_Log::CRIT);
+        }
+    
+        public function info($message, $throwable = null)
+        {
+            $this->doLog($message, $throwable, 'info', Zend_Log::NOTICE);
+        }
+    
+        public function trace($message, $throwable = null)
+        {
+            $this->doLog($message, $throwable, 'trace', Zend_Log::DEBUG);
+        }
+    
+        public function warn($message, $throwable = null)
+        {
+            $this->doLog($message, $throwable, 'warn', Zend_Log::WARN);
+        }
+    
+        /**
+         * Internal dispatcher for the called log method.
+         * @param $message
+         * @param $throwable
+         * @param $log4phpMethod
+         * @param $zendLevel
+         */
+        private function doLog($message, $throwable, $log4phpMethod, $zendLevel)
+        {
+            if (self::$_isLog4phpUsed) {
+                $this->_loggerLog4php->$log4phpMethod($message, $throwable);
+            } else {
+                Mage::log($this->_name . ': ' . $message, $zendLevel);
+                if ($throwable instanceof Exception) {
+                    Mage::logException($throwable);
+                }
+            }
+        }
+    }
+
--- app/code/community/Nmmlm/Log/Logger.php
+++ app/code/community/Nmmlm/Log/Logger.php
@@ -1,28 +1,7 @@
 <?php
 /**
- * Copyright (c) 2010, F. Lancer, SIA
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
- *  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following
- *      disclaimer.
- *  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
- *      following disclaimer in the documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-/**
  * Override original logger to adopt it to Magento.
- * User: Flancer
- * Date: 9/20/12
- * Time: 3:24 AM
+ * Authors: Alex Gusev <alex@flancer64.com>
  */
 /* add log4php folders to include path */
 $inc_base = Mage::getBaseDir('lib') . DS . 'Log4php';
@@ -35,68 +14,65 @@ $add_path .= $inc_base . DS . 'pattern' . PS;
 $add_path .= $inc_base . DS . 'layouts' . PS;
 $add_path .= $inc_base . DS . 'renderers';
 set_include_path(get_include_path() . PS . $add_path);
+
 //
-class Nmmlm_Log_Logger extends Logger
-{
-    private static $isInitialized = false;
+class Nmmlm_Log_Logger extends Logger {
+	private static $_isInitialized = false;
 
-    /**
-     * Override getter to use '$log = Nmmlm_Log_Logger::getLogger($this)' form in Mage classes.
-     * @static
-     *
-     * @param string $name
-     *
-     * @return Logger
-     */
-    public static function getLogger($name)
-    {
-        // init logger
-        if (!Nmmlm_Log_Logger::$isInitialized) {
-            Nmmlm_Log_Logger::initMageLogger();
-        }
-        // define logger name
-        if (is_object($name)) {
-            return parent::getLogger(Nmmlm_Log_Logger::rewriteName(get_class($name)));
-        } else {
-            return parent::getLogger(Nmmlm_Log_Logger::rewriteName($name));
-        }
-    }
+	/**
+	 * Override getter to use '$log = Nmmlm_Log_Logger::getLogger($this)' form in Mage classes.
+	 * @static
+	 *
+	 * @param string $name
+	 *
+	 * @return Logger
+	 */
+	public static function getLogger($name) {
+		// init logger
+		if(!Nmmlm_Log_Logger::$_isInitialized) {
+			Nmmlm_Log_Logger::initMageLogger();
+		}
+		// define logger name
+		if(is_object($name)) {
+			return parent::getLogger(Nmmlm_Log_Logger::rewriteName(get_class($name)));
+		} else {
+			return parent::getLogger(Nmmlm_Log_Logger::rewriteName($name));
+		}
+	}
 
-    private static function initMageLogger()
-    {
-        $dir = Mage::getBaseDir('base');
-        $file = $dir . DS . Nmmlm_Log_Logger::cfgLog4phpConfigFile();
-        Nmmlm_Log_Logger::configure($file);
-        Nmmlm_Log_Logger::$isInitialized = true;
-    }
+	/**
+	 * Load Log4php configuration file.
+	 */
+	private static function initMageLogger() {
+		$dir  = Mage::getBaseDir('base');
+		$file = $dir . DS . Nmmlm_Log_Logger::cfgLog4phpConfigFile();
+		Nmmlm_Log_Logger::configure($file);
+		Nmmlm_Log_Logger::$_isInitialized = true;
+	}
 
-    private static function cfgLog4phpConfigFile()
-    {
-        return (string)Mage::getStoreConfig('dev/log/nmmlm_log4php_config_file');
-    }
+	/**
+	 * Return path to Log4php configuration file relative to Magento base directory.
+	 * @return string
+	 */
+	private static function cfgLog4phpConfigFile() {
+		return (string)Mage::getStoreConfig('dev/log/nmmlm_log4php_config_file');
+	}
 
-    public function __construct($name)
-    {
-        parent::__construct(Nmmlm_Log_Logger::rewriteName($name));
-    }
+	public function __construct($name) {
+		parent::__construct(Nmmlm_Log_Logger::rewriteName($name));
+	}
 
-    public function setName($name)
-    {
-        parent::setName(Nmmlm_Log_Logger::rewriteName($name));
-    }
-
-    /**
-     * Convert Magento style package (Company_Module_Directory_Class) and PHP namespace (Company\Module\Directory\Class) to log4php style package (Company.Module.Directory.Class).
-     * @static
-     *
-     * @param $name
-     *
-     * @return mixed
-     */
-    private static function rewriteName($name)
-    {
-        return str_replace('\\', '.', str_replace('_', '.', $name));
-    }
+	/**
+	 * Convert Magento style package (Company_Module_Directory_Class) and PHP namespace (Company\Module\Directory\Class) to log4php style package (Company.Module.Directory.Class).
+	 * @static
+	 *
+	 * @param $name
+	 *
+	 * @return mixed
+	 */
+	private static function rewriteName($name) {
+		return str_replace('\\', '.', str_replace('_', '.', $name));
+	}
 
 }
 
