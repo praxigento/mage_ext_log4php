a5f44738f90d9e07723d9f9699eaf089ad4880da
Vladimir <vladimirg@inbox.lv>
2015-02-10 00:40:52 +0200

    Deploy script changed from copy to xcopy

--- test/deploy.bat
+++ test/deploy.bat
@@ -1,4 +1,4 @@
 mkdir mage
 composer install
 rem Actions after deploy (database creation, files copy, etc)
-copy ..\src\app\etc\nmmlm mage\app\etc
+xcopy ..\src\app\etc\nmmlm mage\app\etc\nmmlm /E /Y /I
